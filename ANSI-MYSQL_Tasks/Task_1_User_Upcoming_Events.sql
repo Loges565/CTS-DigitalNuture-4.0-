b14024d7b79efe9323abf17b201525ff9511d176
Loges565 <162884286+Loges565@users.noreply.github.com>
2025-05-27 19:42:13 +0530

    Create Task_1_User_Upcoming_Events.sql

---
+++ ANSI-MYSQL_Tasks/Task_1_User_Upcoming_Events.sql
@@ -0,0 +1,13 @@
+
+-- Show a list of all upcoming events a user is registered for in their city, sorted by date.
+SELECT 
+    e.event_id, 
+    e.title, 
+    e.start_date
+FROM Registrations r
+JOIN Users u ON r.user_id = u.user_id
+JOIN Events e ON r.event_id = e.event_id
+WHERE e.status = 'upcoming'
+  AND u.city = e.city
+  AND u.user_id = ?  
+ORDER BY e.start_date ASC;
